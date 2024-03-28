a10921a58442c33874df7596c65e2411a72dc2d7
mrumrill <164077429+mrumrill@users.noreply.github.com>
2024-03-28 09:44:46 -0400

    Update chess_game.py

--- chess_game.py
+++ chess_game.py
@@ -13,28 +13,9 @@ class ChessGame:
         
         return board
 
-
-    #def is_game_over(self):
-        # Implement logic to check if the game is over
-        #optimal = Optimal()
-
-       # if optimal.best_move() is None:
-       #     return True
-       # else:
-       #     return False
-
     def get_current_player(self):
         return self.current_player
 
     def get_board(self):
         return self.board
-
-    #def make_move(self, move):
-        # Implement logic to make a move on the board
-        #optimal = Optimal()
-        #optimal.engine.make_moves_from_current_position([move])
-
-    def get_game_result(self):
-        # Implement logic to determine the game result (checkmate, stalemate, etc.)
-        pass
 
